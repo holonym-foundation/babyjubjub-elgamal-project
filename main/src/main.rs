b67ce0727d2bb4556493ca2cf0163aea099691da
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-02-03 16:38:40 -0500

    should work  with CLI for encyrpt and decypt

--- main/src/main.rs
+++ main/src/main.rs
@@ -3,8 +3,8 @@ use clap::{Subcommand, Args, Parser};
 use ff::PrimeField;
 use std::env;
 // use issuer::Issuer;
-use babyjubjub_rs::{Fr, PrivateKey, ElGamalEncryption, B8, new_key, Point};
-use num_bigint::BigInt;
+use babyjubjub_rs::{Fr, PrivateKey, ElGamalEncryption, B8, new_key, Point, encrypt_elgamal, Q};
+use num_bigint::{BigInt, ToBigInt, BigUint, RandBigInt};
 
 /// BabyJubJub ElGamal
 #[derive(Parser)]
@@ -66,8 +66,19 @@ fn main() {
 
     let cli = Cli::parse();
     let res = match &cli.command {
-        Commands::Encrypt(e) => Err("not implemented"),
-        Commands::Decrypt(d) => Ok(private_key.decrypt_elgamal(ElGamalEncryption {
+        Commands::Encrypt(e) => println!("{:?}", encrypt_elgamal(
+            Point {
+                x: Fr::from_str(&e.pkx).unwrap(),
+                y: Fr::from_str(&e.pky).unwrap()
+            },
+
+            rand::thread_rng().gen_bigint_range(&0.to_bigint().unwrap() , &Q), 
+            &Point {
+                x: Fr::from_str(&e.mx).unwrap(),
+                y: Fr::from_str(&e.my).unwrap()
+            }
+        )),
+        Commands::Decrypt(d) => println!("{:?}", private_key.decrypt_elgamal(ElGamalEncryption {
             c1: Point { 
                 x: Fr::from_str(&d.c1x).unwrap(),
                 y: Fr::from_str(&d.c1y).unwrap(),
@@ -79,5 +90,5 @@ fn main() {
         }))
     };    
 
-    println!("result {:?}", res.unwrap());
+    
 }
