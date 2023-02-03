e85fb4cccac19a9aa30e3e5cb57adba6bd7fad74
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-02-03 15:30:56 -0500

    commented out code

--- issuer-wasm-bindings/src/lib.rs
+++ issuer-wasm-bindings/src/lib.rs
@@ -1,12 +1,12 @@
-use wasm_bindgen::prelude::*;
-use issuer::Issuer;
-extern crate console_error_panic_hook;
-use std::panic;
+// use wasm_bindgen::prelude::*;
+// use issuer::Issuer;
+// extern crate console_error_panic_hook;
+// use std::panic;
 
-#[wasm_bindgen]
-pub fn issue(private_key: String, field1: String, field2: String) -> String {
-    panic::set_hook(Box::new(console_error_panic_hook::hook));
-    let iss = Issuer::from_privkey(&private_key);
-    let sig = iss.issue([field1, field2]).unwrap();
-    serde_json::to_string(&sig).unwrap()
-}
+// #[wasm_bindgen]
+// pub fn issue(private_key: String, field1: String, field2: String) -> String {
+//     panic::set_hook(Box::new(console_error_panic_hook::hook));
+//     let iss = Issuer::from_privkey(&private_key);
+//     let sig = iss.issue([field1, field2]).unwrap();
+//     serde_json::to_string(&sig).unwrap()
+// }
