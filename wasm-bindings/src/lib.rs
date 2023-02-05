9d95b61aa9ad56aa8eb58988d92516d56731d423
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-02-05 14:08:48 -0500

    switched snake case to camel case

--- wasm-bindings/src/lib.rs
+++ wasm-bindings/src/lib.rs
@@ -11,24 +11,24 @@ use std::panic;
 
 
 #[wasm_bindgen]
-pub fn enable_errors() {
+pub fn enableErrors() {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
 }
 
 #[wasm_bindgen]
-pub fn msg_to_point(m: String) -> String {
+pub fn msgToPoint(m: String) -> String {
     let m_big = BigInt::from_str(&m).unwrap();
     let p = Point::from_msg_vartime(&m_big);
     serde_json::to_string(&p).unwrap()
 }
 
 #[wasm_bindgen]
-pub fn point_to_msg(x: String, y: String) -> String {
+pub fn pointToMsg(x: String, y: String) -> String {
     Point::from_xy_strings(x, y).to_msg().to_dec_string()
 }
 
 #[wasm_bindgen]
-pub fn encrypt_point(msg_x: String, msg_y: String, pub_x: String, pub_y: String, nonce: String) -> String {
+pub fn encryptPoint(msg_x: String, msg_y: String, pub_x: String, pub_y: String, nonce: String) -> String {
     let msg = Point::from_xy_strings(msg_x, msg_y);
     let pk = Point::from_xy_strings(pub_x, pub_y);
     let nonce_big: BigInt = BigInt::from_str(&nonce).unwrap();
@@ -37,7 +37,7 @@ pub fn encrypt_point(msg_x: String, msg_y: String, pub_x: String, pub_y: String,
 }
 
 #[wasm_bindgen]
-pub fn decrypt_to_point(privkey: String, c1x: String, c1y: String, c2x: String, c2y: String) -> String {
+pub fn decryptPoint(privkey: String, c1x: String, c1y: String, c2x: String, c2y: String) -> String {
     let prv = PrivateKey::import(
         hex::decode(privkey)
         .unwrap(),
