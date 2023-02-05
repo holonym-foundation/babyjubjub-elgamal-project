0e51745204638200bf8862299a9813295057e1ef
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-02-05 13:46:44 -0500

    better interface for wasm bindings

--- wasm-bindings/src/lib.rs
+++ wasm-bindings/src/lib.rs
@@ -2,10 +2,18 @@ use std::str::FromStr;
 
 use wasm_bindgen::prelude::*;
 use num_bigint::BigInt;
-use babyjubjub_rs::{Fr, Point, ElGamalEncryption, encrypt_elgamal};
+use babyjubjub_rs::{Point, ToDecimalString, ElGamalEncryption, encrypt_elgamal, PrivateKey};
+
 extern crate console_error_panic_hook;
+use std::panic;
 
 // Note: no constant-time gaurantees
+
+
+#[wasm_bindgen]
+pub fn enable_errors() {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+}
 
 #[wasm_bindgen]
 pub fn msg_to_point(m: String) -> String {
@@ -16,7 +24,7 @@ pub fn msg_to_point(m: String) -> String {
 
 #[wasm_bindgen]
 pub fn point_to_msg(x: String, y: String) -> String {
-    Point::from_xy_strings(x, y).to_msg().to_string()
+    Point::from_xy_strings(x, y).to_msg().to_dec_string()
 }
 
 #[wasm_bindgen]
@@ -28,7 +36,18 @@ pub fn encrypt_point(msg_x: String, msg_y: String, pub_x: String, pub_y: String,
     serde_json::to_string(&e).unwrap()
 }
 
-// #[wasm_bindgen]
-// pub fn decrypt_to_point() -> String {
-
-// }
+#[wasm_bindgen]
+pub fn decrypt_to_point(privkey: String, c1x: String, c1y: String, c2x: String, c2y: String) -> String {
+    let prv = PrivateKey::import(
+        hex::decode(privkey)
+        .unwrap(),
+    ).unwrap();
+    let c1 = Point::from_xy_strings(c1x, c1y);
+    let c2 = Point::from_xy_strings(c2x, c2y);
+    let e = ElGamalEncryption {
+        c1: c1,
+        c2: c2
+    };
+    let d = prv.decrypt_elgamal(e);
+    serde_json::to_string(&d).unwrap()
+}
