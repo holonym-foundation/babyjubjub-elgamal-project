b0448045a1e0b5a1f27cc199a0be109eee783b5d
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-02-05 11:52:27 -0500

    wasm bindings compile

--- wasm-bindings/src/lib.rs
+++ wasm-bindings/src/lib.rs
@@ -1,23 +1,34 @@
+use std::str::FromStr;
+
 use wasm_bindgen::prelude::*;
-use babyjubjub_rs::{PrivateKey, };
+use num_bigint::BigInt;
+use babyjubjub_rs::{Fr, Point, ElGamalEncryption, encrypt_elgamal};
 extern crate console_error_panic_hook;
-use std::panic;
+
+// Note: no constant-time gaurantees
 
 #[wasm_bindgen]
-pub fn encrypt_point() -> ElGamalEncryption {
-
+pub fn msg_to_point(m: String) -> String {
+    let m_big = BigInt::from_str(&m).unwrap();
+    let p = Point::from_msg_vartime(&m_big);
+    serde_json::to_string(&p).unwrap()
 }
 
 #[wasm_bindgen]
-pub fn decrypt_point() -> Point {
-
-}
-#[wasm_bindgen]
-pub fn msg_to_point() -> Point {
-
+pub fn point_to_msg(x: String, y: String) -> String {
+    Point::from_xy_strings(x, y).to_msg().to_string()
 }
 
 #[wasm_bindgen]
-pub fn point_to_msg() -> BigInt {
-
+pub fn encrypt_point(msg_x: String, msg_y: String, pub_x: String, pub_y: String, nonce: String) -> String {
+    let msg = Point::from_xy_strings(msg_x, msg_y);
+    let pk = Point::from_xy_strings(pub_x, pub_y);
+    let nonce_big: BigInt = BigInt::from_str(&nonce).unwrap();
+    let e = encrypt_elgamal(&pk, &nonce_big, &msg);
+    "hey".to_string()// serde_json::to_string(e)
 }
+
+// #[wasm_bindgen]
+// pub fn decrypt_to_point() -> String {
+
+// }
