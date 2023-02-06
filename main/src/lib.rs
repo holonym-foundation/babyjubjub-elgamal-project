25c1f26719fdc4ec814c5a3e7530be91ccce2708
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-02-05 21:24:28 -0500

    more comprehensive tests for lagrange interpolation

--- main/src/lib.rs
+++ main/src/lib.rs
@@ -20,7 +20,7 @@ pub fn lagrange_basis_at_0(i: u32, n:u32) -> Fr {
     assert!(n > 0, "n must be greater than 0");
     let one = Fr::one();
     let mut acc = one.clone();
-    let mut j: u32 = 0;
+    let mut j: u32 = 1;
     let i_ = Fr::from_str(&i.to_string()).unwrap();
     // since we are evaluating L_i(x) where x=0, can set x to 0 in formula for lagrange basis. Formula becomes becomes product of j / (j-i) for all j not equal to i
     while j <= n {
@@ -36,11 +36,13 @@ pub fn lagrange_basis_at_0(i: u32, n:u32) -> Fr {
         let mut denominator = j_.clone();
         denominator.sub_assign(&i_);
         
+        println!("acc is {:?}", acc);
         acc.mul_assign(&j_);
+
         println!("denominator {:?}", denominator);
         println!("inverse {:?}", denominator.inverse().unwrap());
         acc.mul_assign(&denominator.inverse().unwrap());
-
+        println!("acc is now {:?}", acc);
         j+=1;
     }
 
@@ -62,13 +64,63 @@ impl ThresholdDecryptor {
 
 #[cfg(test)]
 mod tests {
-    use num_traits::ToPrimitive;
-    use crate::lagrange_basis_at_0;
+    use super::*;
     #[test]
     fn test_lagrange_basis_at_0() {
-        // test for reconstructing y-intercept of line with points (1,5) and (2,6). y-intercept is
+        // test for reconstructing y-intercept of line with points (1,5) and (2,6). test that y-intercept is 4
         let n: u32 = 2; // n  =  number of shares  =  degree of polynomial + 1
         let l1 = lagrange_basis_at_0(1 as u32, n);
         let l2 = lagrange_basis_at_0(2 as u32, n);
+
+        println!("\n\nl1 l2\n\n{:?}\n{:?}\n\n", l1, l2);
+
+        let y1 = Fr::from_str("5").unwrap();
+        let y2 = Fr::from_str("6").unwrap();
+        // calculate l1y1+l2y2
+
+        // part 1
+        let mut result = l1.clone();
+        result.mul_assign(&y1);
+        
+        // part 2
+        let mut part2 = l2.clone();
+        part2.mul_assign(&y2);
+        
+        result.add_assign(&part2);
+
+        assert!(result.eq(&Fr::from_str("4").unwrap()));
+
+
+
+        // Now, try the same thing but for a degree-3 polynomial: 3x^2+100x+123. Points are (1, 226), (2, 335) and (3, 450)
+        // test for reconstructing y-intercept of line with points (1,5) and (2,6). test that y-intercept is 4
+        let n: u32 = 3; // n  =  number of shares  =  degree of polynomial + 1
+        let l1 = lagrange_basis_at_0(1 as u32, n);
+        let l2 = lagrange_basis_at_0(2 as u32, n);
+        let l3 = lagrange_basis_at_0(3 as u32, n);
+
+        println!("\n\nl1 l2 l3\n\n{:?}\n{:?}\n{:?}\n\n", l1, l2, l3);
+        let y1 = Fr::from_str("226").unwrap();
+        let y2 = Fr::from_str("335").unwrap();
+        let y3 = Fr::from_str("450").unwrap();
+        // calculate l1y1+l2y2
+
+        // part 1
+        let mut result = l1.clone();
+        result.mul_assign(&y1);
+        
+        // part 2
+        let mut part2 = l2.clone();
+        part2.mul_assign(&y2);
+
+        // part 3
+        let mut part3 = l3.clone();
+        part3.mul_assign(&y3);
+        
+        
+        result.add_assign(&part2);
+        result.add_assign(&part3);
+        assert!(result.eq(&Fr::from_str("123").unwrap()));
+        
     }
 }
