09060ab4bcc95bb98ba8147b386f8825ed889b11
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-02-13 18:58:10 +0100

    added the tests from earlier repository

--- wasm-bindings/test-js/tests.js
+++ wasm-bindings/test-js/tests.js
@@ -1,1 +1,21 @@
-console.log("hˆélo wörld")
+const { expect } = require("chai");
+const { Auditor, Lit, Encryption  } = require("../src-js/wrapper");
+
+describe("Encryption", function() {
+    before(async function () {
+        this.auditor = new Auditor(Buffer.from("abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890", "hex"));
+        this.lit = new Lit(Buffer.from("11112222333344444555566667777888899990000aaaabbbbccccddddeeeeffff", "hex"));
+        this.auditorKeygenForLit = await this.auditor.keygen();
+        this.litKeygenForAuditor = await this.lit.keygen();
+        this.auditorPubkey = await this.auditor.pubkey(this.litKeygenForAuditor);
+        this.litPubkey = await this.lit.pubkey(this.auditorKeygenForLit);
+        this.encryption = new Encryption(this.litPubkey, this.auditorPubkey);
+    });
+    it("encrypt -> decrypt", async function() {
+        const msg = 12345678901234567890n
+        const encrypted = await this.encryption.encrypt(msg.toString());
+        const litPartialDecryption = await this.lit.partialDecrypt(this.auditorKeygenForLit, encrypted.encrypted.c1);
+        const fullDecryption = await this.auditor.decrypt(this.litKeygenForAuditor, encrypted.encrypted, litPartialDecryption);
+        expect(msg.toString()).to.equal(fullDecryption);
+    })
+});
