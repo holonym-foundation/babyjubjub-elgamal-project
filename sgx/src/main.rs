cd914e76badc56ace28bbd1d442e10f906d2b7e4
Ubuntu <sgxtest@sgxtestv2.rjxarxuxhm1eth3vwxfj3h2txe.bx.internal.cloudapp.net>
2023-03-19 18:45:48 +0000

    successfully sealing and unsealing PrivateKey

--- sgx/src/main.rs
+++ sgx/src/main.rs
@@ -70,22 +70,41 @@ pub fn recover_seal_key(s: Seal) -> Result<[u8; 16], ErrorCode> {
     egetkey(s.label, &s.seal_data)
 }
 
+// First argument should be empty if new private key is to be generated. Otherwise, it should be the seal of the private key to be used, as a JSON representation of the Seal object
 fn main() {
     let args: Vec<String> = env::args().collect();
     // Seal key:
     let key: [u8; 16];
+    let seal: Seal;
+    let label: &[u8; 16] = b"Holonym zkEscrow";
     match args.len() {
         0 => {
-            todo!()
+            println!("A Seal wasn't supplied as the first argument - creating new private key. To use a sealed private key, provide a JSON string representing the Seal as the first argument");
+            (key, seal) = get_seal_key_for_label(*label);
+            println!("Generated new private key. If you'd like to use it later, save this JSON object and supply it as the first argument to this script: \n{:?}", serde_json::to_string(&seal).unwrap())
         }
-        _ => todo!()
+        // arg 0 is "enclave" if any arguments is supplied, and will not exist otherwise. So there cannot be just 1 arg, since "enclave" will be an additional argument
+        2 => {
+            println!("Attempting to decrypt from {}", args[1]);
+            seal = match serde_json::from_str(&args[1]) {
+                Ok(s) => s,
+                Err(e) => panic!("Failed to deserialize. Error: {}",e)
+            };
+            key = match recover_seal_key(seal) {
+                Ok(k) => k,
+                Err(e) => panic!("Failed to decrypt. Error: {:?}", e)
+            };
+            println!("Successfully recovered key");
+            
+        }
+        _ => panic!("Please supply exactly 0 or 1 arguments. Supplied {:?} as arguments", args)
     }
-    let r: [u8; 16] = random();
-    let mut recoverd = r.to_vec();
-    recoverd.append(&mut [0 as u8; 16].to_vec());
-    
-    // let p = PrivateKey::import(r.to_vec()).unwrap();
-    println!("heyyyyyy, {:?}", args[0]);   
+    // Create seed for random polynomial (16 bytes is enough randomness; the rest can all be zeros)
+    let mut padded = key.to_vec();
+    padded.append(&mut [0 as u8; 16].to_vec());
+    let p = PrivateKey::import(padded).unwrap();
+    println!("Heyyyyy {:?}", p.public());   
+    println!("Note ^ this public key will be different if the enclave measurements do not match the measurements before. I.e., if the code has changed.");
 }
 
 #[cfg(test)]
