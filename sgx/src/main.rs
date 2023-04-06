eceb702ad1f794f71679ded5444fe18a017d521f
Ubuntu <sgxtest@sgxtestv2.rjxarxuxhm1eth3vwxfj3h2txe.bx.internal.cloudapp.net>
2023-04-06 04:38:03 +0000

    added ability to seal / recover communication key

--- sgx/src/main.rs
+++ sgx/src/main.rs
@@ -15,13 +15,12 @@ mod sealing;
 struct Args {
     #[arg(short, long, value_name = "NODE_IDX")] 
     idx: usize,
-    #[arg(short, long, value_name = "SECRET_SHARE_SEAL")] 
+    #[arg(short, long, value_name = "SECRET_keygen_SEAL")] 
     share: Option<String>,
     #[arg(short, long, value_name = "P2P_SECRET_KEY_SEAL")] 
-    communication: Option<String>
+    comms: Option<String>
 }
 
-// First argument should be empty if new private key is to be generated. Otherwise, it should be the seal of the private key to be used, as a JSON representation of the Seal object
 fn main() {
     // use hyper::{Client, Uri};
 
@@ -35,9 +34,9 @@ fn main() {
     // let args: Vec<String> = env::args().collect();
     
     // Seal key:
-    let key_share: [u8; 16];
-    let seal_share: Seal;
-    let label_share: &[u8; 16] = b"secretshare seal";
+    let key_keygen: [u8; 16];
+    let seal_keygen: Seal;
+    let label_keygen: &[u8; 16] = b"secretshare seal";
 
     let key_comms: [u8; 16];
     let seal_comms: Seal;
@@ -45,55 +44,48 @@ fn main() {
 
     let args = Args::parse();
 
-    // let comms: Comms;
+    // Reconstruct node from keygen seal
     if let Some(s) = args.share {
-        seal_share = match serde_json::from_str(&s) {
+        seal_keygen = match serde_json::from_str(&s) {
                         Ok(deser) => deser,
-                        Err(e) => panic!("Failed to deserialize secret share's seal. Error: {}",e)
+                        Err(e) => panic!("Failed to deserialize keygen seal. Error: {}",e)
                     };
-        key_share = match recover_seal_key(seal_share) {
+        key_keygen = match recover_seal_key(seal_keygen) {
             Ok(k) => k,
-            Err(e) => panic!("Failed to decrypt secret share's seal. Error: {:?}", e)
+            Err(e) => panic!("Failed to decrypt keygen seal. Error: {:?}", e)
         };
-        println!("Successfully recovered secret share from seal");
+        println!("Successfully recovered keygen from seal");
     } else {
-        println!("A Seal wasn't supplied as the first argument - creating new private key. To use a sealed private key, provide a JSON string representing the Seal as the first argument");
-        (key_share, seal_share) = get_seal_key_for_label(*label_share);
-        println!("Generated new private key. If you'd like to use it later, save this JSON object and supply it as the first argument to this script: \n{:?}", serde_json::to_string(&seal_share).unwrap())
+        println!("A keygen Seal wasn't supplied- creating new keygen. To use a sealed private key, provide a JSON string representing the Seal as the first argument");
+        (key_keygen, seal_keygen) = get_seal_key_for_label(*label_keygen);
+        println!("Generated new keygen. If you'd like to use it later, save this JSON object and supply it as the first argument to this script: \n{:?}", serde_json::to_string(&seal_keygen).unwrap())
     }
 
-    // Create the Node struct from the secret share
+    // Create the Node struct from the keygen share
     // Create seed for random polynomial (16 bytes is enough randomness; the rest can all be zeros)
-    let mut padded = key_share.to_vec();
+    let mut padded = key_keygen.to_vec();
     padded.append(&mut [0 as u8; 16].to_vec());
     // let p = PrivateKey::import(padded).unwrap();
     let node = Node::init_from_seed(&padded, args.idx, 2, 2);  
-    println!("This nodes' public key : {:?}", node.pubkey_share()); 
-    println!("Note ^ this public key will be different if the enclave measurements do not match the measurements before. I.e., if the code has changed.");
+    println!("This nodes' public keygen is : {:?}", node.pubkey_share()); 
+    println!("Note ^ this public keygen will be different if the enclave measurements do not match the measurements before. I.e., if the code has changed.");
 
-
-    // match args.len() {
-    //     0 => {
-    //         println!("A Seal wasn't supplied as the first argument - creating new private key. To use a sealed private key, provide a JSON string representing the Seal as the first argument");
-    //         (key, seal) = get_seal_key_for_label(*label);
-    //         println!("Generated new private key. If you'd like to use it later, save this JSON object and supply it as the first argument to this script: \n{:?}", serde_json::to_string(&seal).unwrap())
-    //     }
-    //     // arg 0 is "enclave" if any arguments is supplied, and will not exist otherwise. So there cannot be just 1 arg, since "enclave" will be an additional argument
-    //     2 => {
-    //         println!("Attempting to decrypt from {}", args[1]);
-    //         seal = match serde_json::from_str(&args[1]) {
-    //             Ok(s) => s,
-    //             Err(e) => panic!("Failed to deserialize. Error: {}",e)
-    //         };
-    //         key = match recover_seal_key(seal) {
-    //             Ok(k) => k,
-    //             Err(e) => panic!("Failed to decrypt. Error: {:?}", e)
-    //         };
-    //         println!("Successfully recovered key");
-            
-    //     }
-    //     _ => panic!("Please supply exactly 0 or 1 arguments. Supplied {:?} as arguments", args)
-    // }
+    // Reconstruct Comms from communication key seal
+    if let Some(s) = args.comms {
+        seal_comms = match serde_json::from_str(&s) {
+                        Ok(deser) => deser,
+                        Err(e) => panic!("Failed to deserialize communication key seal. Error: {}",e)
+                    };
+        key_comms = match recover_seal_key(seal_comms) {
+            Ok(k) => k,
+            Err(e) => panic!("Failed to decrypt communication key seal. Error: {:?}", e)
+        };
+        println!("Successfully recovered communication private key from seal");
+    } else {
+        println!("A communication key Seal wasn't supplied - creating new communication private key. To use a sealed private key, provide a JSON string representing the Seal as the first argument");
+        (key_comms, seal_comms) = get_seal_key_for_label(*label_comms);
+        println!("Generated new communication secret key. If you'd like to use it later, save this JSON object and supply it as the first argument to this script: \n{:?}", serde_json::to_string(&seal_comms).unwrap())
+    }
     
     
 }
