e8d1e7da2110a4b90a4c7a04f3c1b38dcfe4f86d
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-04-27 11:39:57 -0700

    bindings

--- mpc-core-wasm-bindings/src/lib.rs
+++ mpc-core-wasm-bindings/src/lib.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use babyjubjub_elgamal::Node;
+use babyjubjub_rs::{PrivateKey, Point};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Encryptor { }
+#[wasm_bindgen]
+pub struct Decryptor {
+    node: Node,
 }
+
+impl Encryptor {
+    // TODO
+}
+
+impl Decryptor {
+    pub fn from_32_byte_hex_key(hex_key: String, idx: usize, threshold_nodes: usize, total_nodes: usize) -> Decryptor {
+        // let private_key = PrivateKey::import(
+        //     hex::decode(hex_key).unwrap(),
+        // ).unwrap();
+        Decryptor {
+            node : Node::init_from_seed(
+                &hex::decode(hex_key).unwrap(), 
+                idx,
+                threshold_nodes, 
+                total_nodes
+            )
+        }
+        
+    }
+    pub fn decrypt(&self, c1: Point) -> Point {
+        self.node.partial_decrypt(&c1)
+    }
+}
+    
