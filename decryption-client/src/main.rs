98ff7663aa57d87217c4d00241fa1e19510cc0ae
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-05-09 21:44:45 -0400

    PRF server connection works

--- decryption-client/src/main.rs
+++ decryption-client/src/main.rs
@@ -3,22 +3,46 @@ use std::env::{args, self};
 use babyjubjub_rs::{Point, ElGamalEncryption, ToDecimalString};
 use decryptor_node::DecryptionRequest;
 use ethers::types::Signature;
-
-// pub struct PrfRequest {
-//     api_key: String,
-//     prf_in: String
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize)]
+pub struct PrfRequest {
+    #[serde(rename = "API_KEY")]
+    api_key: String,
+    #[serde(rename = "prfIn")]
+    prf_in: String
+}
+// #[derive(Serialize, Deserialize)]
+// pub struct PrfResponse {
+//     #[serde::rename = "prfOut"]
+//     prf_out: String
 // }
 
 fn main() {
-    let json_ciphertext = args().nth(1).unwrap();
-    let ciphertext: ElGamalEncryption = serde_json::from_str(&json_ciphertext).unwrap(); 
-    let private_key: Result<String, env::VarError> = env::var("ZK_ESCROW_AUTHORITY_PRIVATE_KEY");
-    let api_key = env::var("ZK_ESCROW_AUTHORITY_PRIVATE_KEY");
+    // Uncomment:
+        // let json_ciphertext = args().nth(1).unwrap();
+        // let ciphertext: ElGamalEncryption = serde_json::from_str(&json_ciphertext).unwrap(); 
+        // let private_key = env::var("ZK_ESCROW_AUTHORITY_PRIVATE_KEY").expect("Expected ZK_ESCROW_AUTHORITY_PRIVATE_KEY in the environment");
+    let api_key = env::var("ZK_ESCROW_AUTHORITY_API_KEY").expect("Expected ZK_ESCROW_AUTHORITY_API_KEY in the environment");
     // Just decrypt from nodes 1 and 2 for now:
     let nodes_to_decrypt_from: Vec<u32> = vec![1, 2];
     let decryption_reqs = nodes_to_decrypt_from.iter().map(|node_number| {
-        make_signed_decryptreq(&ciphertext, node_number, &nodes_to_decrypt_from)
+        // uncomment: make_signed_decryptreq(&ciphertext, node_number, &nodes_to_decrypt_from)
     });
+    let client = reqwest::blocking::Client::new();
+    let req = PrfRequest {
+        api_key: api_key,
+        prf_in: "69".to_string() //ciphertext.c1.x.to_dec_string()
+    };
+    let prf = client.post("https://prf.zkda.network/authority")
+    .json(&req)
+    .send()
+    .unwrap()
+    .text()
+    .unwrap();
+    println!("response: {:?}", prf);
+    // .json::<PrfResponse>().unwrap();
+    //     .json::<PrfResponse>()?;
+    // println!("PRF output: {}", prf.prfOut);
 
 }
 
@@ -32,3 +56,5 @@ fn make_signed_decryptreq(/*private_key: */ciphertext: &ElGamalEncryption, for_n
         sig: Signature { r: sig.r, s: sig.s, v: sig.v }
     }
 }
+
+// fn get_prf(prf_in: String) -> String {
