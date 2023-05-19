c9a38d03cb415d4baf1fee11ee9b1ea65651d9f0
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-05-19 16:08:00 -0400

    ipv6 restriction, working on rate limiting

--- oprf-server/src/main.rs
+++ oprf-server/src/main.rs
@@ -2,6 +2,8 @@ use std::{env, collections::HashSet, str::FromStr};
 
 use babyjubjub_rs::{Point};
 use num_bigint::{BigInt};
+use rocket::catcher;
+use rocket::request::{FromRequest, Outcome};
 use rocket::{State, serde::json::Json, response::status::BadRequest};
 use rocket::{Request, Response, fairing::{Fairing, Info, Kind}, http::{Header, Status}};
 
@@ -32,9 +34,39 @@ impl Fairing for Cors {
 
     }
 }
+
+#[derive(Debug)]
+pub struct RateLimit {
+    remaining: usize,
+}
+
+#[derive(Debug)]
+pub enum RateLimitError {
+    TooManyRequests,
+    IsntIPv4,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for RateLimit {
+    type Error = RateLimitError;
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let addr = request.remote().unwrap();
+        println!("IP: {:?}", addr.ip());
+        if !addr.is_ipv4() {
+            return Outcome::Failure((Status::TooManyRequests, RateLimitError::IsntIPv4));
+        }
+        Outcome::Success(RateLimit { remaining: 69 })
+    }
+}
+
+#[catch(429)]
+fn rate_limit(r: &Request) -> &'static str {
+    "Too many requests OR request is not from an IPv4 address"
+}
+
 // this route is solely so that a TLS connection can be started early before any user action and automatically cached by both parties. This avoids the handshake latency overhead when the user requests the OPRF
 #[get("/")]
-fn do_nothing() -> &'static str { "GM" }
+fn do_nothing(r: RateLimit) -> &'static str { println!("remaining requests: {:?}", r); "GM" }
 
 #[post("/oprf", format = "json", data = "<point>")]
 fn index(privkey: &State<BigInt>, point: Json<Point>) -> Result<String, BadRequest<&'static str>> {
@@ -55,20 +87,6 @@ fn index(privkey: &State<BigInt>, point: Json<Point>) -> Result<String, BadReque
 
 #[launch]
 fn rocket() -> _ {
-    // Setup CORS
-    // let allowed_origins = AllowedOrigins::some_exact(&["https://silkwallet.net", "http://localhost:3000", "http://127.0.0.1:3000"]);
-    // let cors = rocket_cors::CorsOptions {
-    //     allowed_origins,
-    //     allowed_methods: HashSet::from_iter(
-    //         vec![Method::from_str("POST").unwrap(), Method::from_str("GET").unwrap()]
-    //         .iter().cloned()
-    //     ),
-    //     allowed_headers: AllowedHeaders::some(&["*"]),
-    //     // allow_credentials: true,
-    //     ..Default::default()
-    // }
-    // .to_cors().unwrap();
-
     // Get the private key env var
     let privkey: BigInt = env::var("OPRF_KEY")
         .expect("OPRF_KEY must be an environment variable. It should be a decimal string representing a random integer between 0 and the order of the curve's subgroup.")
@@ -78,4 +96,5 @@ fn rocket() -> _ {
     .manage(privkey)
     .attach(Cors)
     .mount("/", routes![index, do_nothing])
+    .register("/", catchers![rate_limit])
 }
