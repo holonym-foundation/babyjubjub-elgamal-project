b224cb86e9f3facf75d392d88265c079c62321e5
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-05-19 17:11:49 -0400

    added redis calls and refactored cors

--- oprf-server/src/main.rs
+++ oprf-server/src/main.rs
@@ -4,9 +4,10 @@ use babyjubjub_rs::{Point};
 use num_bigint::{BigInt};
 use rocket::catcher;
 use rocket::request::{FromRequest, Outcome};
+use rocket::time::Instant;
 use rocket::{State, serde::json::Json, response::status::BadRequest};
 use rocket::{Request, Response, fairing::{Fairing, Info, Kind}, http::{Header, Status}};
-
+use redis::{Commands, RedisError, Connection, Client};
 // use rocket_contrib::json::Json;use std::env;
 #[macro_use] extern crate rocket;
 
@@ -22,12 +23,15 @@ impl Fairing for Cors {
         }
     }
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        let _origin = _request.headers().get_one("origin").unwrap();
-        let origin = if ALLOW_ORIGINS.contains(&_origin) { _origin } else { "null" };
+        let _origin = match _request.headers().get_one("origin") {
+            Some(origin) => origin,
+            None => "null"
+        };
+        let allow_origin = if ALLOW_ORIGINS.contains(&_origin) { _origin } else { "null" };
 
         response.set_status(Status::new(200));
         
-        response.set_header(Header::new("Access-Control-Allow-Origin", origin)); //CHANGE THIS TO ONLY SAFE SITES
+        response.set_header(Header::new("Access-Control-Allow-Origin", allow_origin)); //CHANGE THIS TO ONLY SAFE SITES
         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
@@ -35,9 +39,13 @@ impl Fairing for Cors {
     }
 }
 
+const REQUESTS_PER_IP_PER_INTERVAL: u8 = 5;
+const TOTAL_REQUESTS_PER_INTERVAL: usize = 1000;
+const INTERVAL: usize = 60; // seconds
+
 #[derive(Debug)]
 pub struct RateLimit {
-    remaining: usize,
+    remaining: u8,
 }
 
 #[derive(Debug)]
@@ -51,10 +59,29 @@ impl<'r> FromRequest<'r> for RateLimit {
     type Error = RateLimitError;
     async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
         let addr = request.remote().unwrap();
-        println!("IP: {:?}", addr.ip());
+        let ip = addr.ip();
+        println!("IP: {:?}", ip);
         if !addr.is_ipv4() {
             return Outcome::Failure((Status::TooManyRequests, RateLimitError::IsntIPv4));
         }
+        // let redis = request.guard::<State<&mut Connection>>();
+        // let redis = request.rocket().state::<&mut redis::Connection>().unwrap();
+        let client = request.rocket().state::<redis::Client>().unwrap();
+        let now = Instant::now();
+        let mut redis = client.get_connection().unwrap();
+        println!("time to get redis connection: {:?}", now.elapsed());
+
+        let recent_requests: u8 = match redis.get::<String,u8>(ip.to_string()) {
+            Ok(num) => { 
+                let _: () = redis.incr(ip.to_string(), 1).unwrap();
+                num
+            },
+            RedisError =>  { 
+                let _: String = redis.set(ip.to_string(), 1).unwrap(); 
+                0
+            }
+        };
+        
         Outcome::Success(RateLimit { remaining: 69 })
     }
 }
@@ -92,8 +119,19 @@ fn rocket() -> _ {
         .expect("OPRF_KEY must be an environment variable. It should be a decimal string representing a random integer between 0 and the order of the curve's subgroup.")
         .parse::<BigInt>()
         .unwrap();
+    let redis = redis::Client::open("redis://127.0.0.1")
+                        .unwrap();
+
+    // let mut client = redis
+    //                     .get_connection()
+    //                     .unwrap();
+
+    // let _ : redis::RedisResult<String> = redis.set("redisworks", "yes");
+    // let result: String = redis.get("redisworks").unwrap();
+    // assert_eq!(result, "yes", "could not connect to redis");
     rocket::build()
     .manage(privkey)
+    .manage(redis)
     .attach(Cors)
     .mount("/", routes![index, do_nothing])
     .register("/", catchers![rate_limit])
