48a9ea6cffa32d03e7b2fb17d2a8cd3385f4a137
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-05-19 18:39:24 -0400

    rate limit working

--- oprf-server/src/main.rs
+++ oprf-server/src/main.rs
@@ -39,9 +39,9 @@ impl Fairing for Cors {
     }
 }
 
-const REQUESTS_PER_IP_PER_INTERVAL: u8 = 5;
+const REQUESTS_PER_IP_PER_INTERVAL: u8 = 3;
 const TOTAL_REQUESTS_PER_INTERVAL: usize = 1000;
-const INTERVAL: usize = 60; // seconds
+const INTERVAL: usize = 9; // seconds
 
 #[derive(Debug)]
 pub struct RateLimit {
@@ -67,22 +67,29 @@ impl<'r> FromRequest<'r> for RateLimit {
         // let redis = request.guard::<State<&mut Connection>>();
         // let redis = request.rocket().state::<&mut redis::Connection>().unwrap();
         let client = request.rocket().state::<redis::Client>().unwrap();
-        let now = Instant::now();
+        // let now = Instant::now();
         let mut redis = client.get_connection().unwrap();
-        println!("time to get redis connection: {:?}", now.elapsed());
+        // println!("time to get redis connection: {:?}", now.elapsed());
 
+        // let _: () = redis.set_ex(ip.to_string(), 0, INTERVAL).unwrap();
         let recent_requests: u8 = match redis.get::<String,u8>(ip.to_string()) {
             Ok(num) => { 
                 let _: () = redis.incr(ip.to_string(), 1).unwrap();
                 num
             },
             RedisError =>  { 
-                let _: String = redis.set(ip.to_string(), 1).unwrap(); 
+                let _: () = redis.set_ex(ip.to_string(), 1, INTERVAL).unwrap(); 
                 0
             }
         };
         
-        Outcome::Success(RateLimit { remaining: 69 })
+        
+        if recent_requests >= REQUESTS_PER_IP_PER_INTERVAL {
+            Outcome::Failure((Status::TooManyRequests, RateLimitError::TooManyRequests))
+        } else {
+            Outcome::Success(RateLimit { remaining: recent_requests })
+        }
+        
     }
 }
 
